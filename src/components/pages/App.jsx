a9f36eb7af730ee912edbbbe52a10c2ecef10ee5
ilia Lobastov <ilobastov28@gmail.com>
2025-10-02 11:54:39 +0300

    first-screen

---
+++ src/components/pages/App.jsx
@@ -0,0 +1,16 @@
+import BottomNavbar from '../ui/BottomNavbar'
+import SectionOne from '../ui/SectionOne'
+import TopNavbar from '../ui/TopNavbar'
+import './App.css'
+
+function App() {
+  return (
+    <div className='app-container'>
+        <TopNavbar />
+        <BottomNavbar />
+        <SectionOne />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/BottomNavbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function BottomNavbar() {
+  return (
+    <div className="bottom-header">
+      <div className="bottom-header-container">
+        <div className="logo">
+          <img src='/logo.png' alt="uber" />
+          <div>Официальный партнер Uber в Москве</div>
+        </div>
+
+        <div className="contact-info">
+          <div className="call-us">
+            <span>Звоните нам ежедневно</span>
+            <a href="#" className="phone">
+              8 495 797 09 88
+            </a>
+          </div>
+
+          <button className="callback-btn">заказать звонок</button>
+        </div>
+      </div>
+    </div>
+  )
+}
